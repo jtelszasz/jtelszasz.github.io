42006009c65199ba6b470694a6f2854bb0707474
Justin Elszasz <jtelszasz@gmail.com>
2021-03-18 18:16:12 -0400

    fix js again

--- assets/javascript/gtag.js
+++ assets/javascript/gtag.js
@@ -1,8 +1,5 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-ZCDG4C67E9');
-</script>
+gtag('config', 'G-ZCDG4C67E9');
