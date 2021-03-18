f2ea580340866ed9ff78a9d7f27d5fc15be9334f
Justin Elszasz <jtelszasz@gmail.com>
2021-03-18 17:37:17 -0400

    set up google analytics

---
+++ assets/javascript/gtag.js
@@ -0,0 +1,8 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZCDG4C67E9');
+</script>
